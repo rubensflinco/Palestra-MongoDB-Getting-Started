0014c0378df4800143e71e2df4f22d30de9a7fcc
Rubens Flinco <rubens.flinco@outlook.com.br>
2022-02-25 20:31:45 -0300

    Update mongodbFunConfig.js

--- clone/mongodbFun/mongodbFunConfig.js
+++ clone/mongodbFun/mongodbFunConfig.js
@@ -7,5 +7,6 @@ config.mongodb.porta = configGlobal.banco.porta;
 config.mongodb.database = configGlobal.banco.databse;
 config.mongodb.username = configGlobal.banco.user;
 config.mongodb.password = configGlobal.banco.pass;
+config.mongodb.string = configGlobal.banco.string;
 
 module.exports = config;
